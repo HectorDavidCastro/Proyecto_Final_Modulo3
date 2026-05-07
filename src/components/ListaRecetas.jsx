ea0a16ad1894fc8e8418395a2ea49f1257d4e081
HectorDavidCastro <davidcastro5207@gmail.com>
2026-05-07 12:33:27 -0300

    Estructura Inicial

---
+++ src/components/ListaRecetas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ListaRecetas = () => {
+  return (
+    <div>ListaRecetas</div>
+  )
+}
---
+++ src/components/RecetasGuardadas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RecetasGuardadas = () => {
+  return (
+    <div>RecetasGuardadas</div>
+  )
+}
